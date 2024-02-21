edc074276fec790d657d8da232faa427aef0375a
Shikano Hakukaze <hakukano@flp-web.org>
2024-02-20 16:23:37 -0800

    added hls

--- home/.vim/lua/lsp.lua
+++ home/.vim/lua/lsp.lua
@@ -91,6 +91,10 @@ lspconfig.gopls.setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
+lspconfig.hls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
 lspconfig.html.setup({
   capabilities = capabilities,
   on_attach = on_attach,
