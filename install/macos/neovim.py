6f40be4ef0ecc09ac0ae0d2f84014cc914d9110c
Shikano Hakukaze <hakukano@flp-web.org>
2024-03-27 20:41:45 -0700

    remove unnecessary neovim dependencies

--- install/macos/neovim.py
+++ install/macos/neovim.py
@@ -15,21 +15,6 @@ PROGRAMS = {
     'nvim': 'brew install neovim',
     'ag': 'brew install the_silver_searcher',
     'rg': 'cargo install ripgrep --features="pcre2"',
-    'rust-analyzer': 'brew install rust-analyzer',
-    'clang': 'brew install llvm',
-    'texlab': 'brew install texlab',
-    'gopls': 'brew install gopls',
-    'cmake-language-server': 'pip install cmake-language-server',
-    'pylsp': 'pip install python-lsp-server',
-    'bash-language-server': 'yarn global add bash-language-server',
-    'docker-langserver': 'yarn global add dockerfile-language-server-nodejs',
-    'css-languageserver': 'yarn global add vscode-css-languageserver-bin',
-    'html-languageserver': 'yarn global add vscode-html-languageserver-bin',
-    'vscode-json-languageserver': 'yarn global add vscode-json-languageserver',
-    'typescript-language-server':
-        'yarn global add typescript typescript-language-server',
-    'vim-language-server': 'yarn global add vim-language-server',
-    'yaml-language-server': 'yarn global add yaml-language-server',
 }
 
 DEPENDENCIES = [
