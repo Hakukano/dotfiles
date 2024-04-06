508aae5e444f20ce2781fb3c56e129d5e3fbf91a
Shikano Hakukaze <hakukano@flp-web.org>
2024-04-05 22:55:35 -0700

    allow exrc

--- configs/nvim/lua/polish.lua
+++ configs/nvim/lua/polish.lua
@@ -2,6 +2,9 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- Enable local nvim configs
+vim.o.exrc = true
+
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
