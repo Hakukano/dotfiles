e068ddaa8cdc0f8648c60c8be11ba9786431d13e
Hakukaze Shikano <hakukano@flp-web.org>
2025-02-18 09:03:01 -0800

    remove unnecessary file

--- configs/nvim/lua/community.lua
+++ configs/nvim/lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
