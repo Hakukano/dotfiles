7e774624be86631bbd4dbe947e0c4c47fd5edb49
Hakukaze Shikano <hakukano@flp.zone>
2025-03-09 01:25:39 -0800

    updated functions

--- configs/zsh/configs/functions.zsh
+++ configs/zsh/configs/functions.zsh
@@ -6,11 +6,19 @@ exists() {
     if (( $+commands[$1] )); then return 0; else return 1; fi
 }
 
-sdf() {
-    svn diff $@ | diff-so-fancy
-}
-
 # lists zombie processes
 zombie() {
     ps aux | awk '{if ($8=="Z") { print $2 }}'
+}
+
+eachdir() {
+    for dir in `ls -d */`; do
+        dir_name=${dir%%/}
+        echo '<<<<<<<<'
+        pushd ${dir_name}
+        eval "$*"
+        popd
+        echo '>>>>>>>>'
+        echo ''
+    done
 }
